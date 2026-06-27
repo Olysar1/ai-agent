cf44dc33a508382b2524b8cc3a48f7074cf01999
Olysar1 <giorgibarbakadz1@gmail.com>
2026-06-28 02:57:06 +0400

    set up project. add first prompting functionality

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if not api_key:
+    raise ValueError("API KEY was not found.")
+
+client = genai.Client(api_key=api_key)
+
+def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+
+    if not response.usage_metadata:
+        raise ValueError("Request failed.")
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
